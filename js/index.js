b7b56e553a017ee7c2168e86c82bf899abc195bb
Ali Sajad Ahmadi <forwebonly0@gmail.com>
2023-04-16 22:34:22 +0430

    V 1.0

--- js/index.js
+++ js/index.js
@@ -1,24 +1,20 @@
 let inputValue = document.querySelector('input');
 const addBtn = document.querySelector('.addBtn');
 let todos = document.querySelector('#todos');
-const circleEl = document.querySelector('.circle');
 
 function addTodo() {
     // Creates new (li) element
     let newTodoLi = document.createElement('li');
     newTodoLi.innerHTML = inputValue.value;
-    newTodoLi.className = 'todo_item';
 
     // Creates new remove button element
-    let newRemoveBtn = document.createElement('button');
-    newRemoveBtn.innerHTML = 'Remove';
-    newRemoveBtn.className = 'removeBtn';
+    let newRemoveBtn = document.createElement('i');
+    newRemoveBtn.className = 'fa-solid fa-trash';
 
     // Input validation
     if (inputValue.value) {
         newTodoLi.appendChild(newRemoveBtn);
         todos.append(newTodoLi);
-        circleEl.style.display = 'block';
         todos.style.display = 'block';
     } else{
         alert('Add a title');
